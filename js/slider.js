96f3c741758a8e040b0932e28dbbf0532a69766e
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-03-04 10:31:53 +0300

    карточка

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,9 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper(".realState", {
   slidesPerView: 1,
   freeMode: true,
   breakpoints: {},
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
