bad2facd9f026da69f12d5cd4d739a158bb6dc58
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-03-07 10:15:14 +0300

    исправлен текст карточки

--- js/main.js
+++ js/main.js
@@ -49,6 +49,173 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("consultFormMain");
 });
 
+function getUrlParameter(name) {
+  name = name.replace(/[$$$$]/g, "\\$&");
+  let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+  let results = regex.exec(window.location.href);
+  if (!results) return null;
+  if (!results[2]) return "";
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+// Получаем UTM-параметры из URL
+let utm_source = getUrlParameter("utm_source");
+let utm_medium = getUrlParameter("utm_medium");
+let utm_campaign = getUrlParameter("utm_campaign");
+let utm_term = getUrlParameter("utm_term");
+let utm_content = getUrlParameter("utm_content");
+let utm_campaign_name = getUrlParameter("utm_campaign_name");
+let utm_placement = getUrlParameter("utm_placement");
+let utm_device = getUrlParameter("utm_device");
+let utm_region_name = getUrlParameter("utm_region_name");
+let utm_position = getUrlParameter("utm_position");
+let utm_position_type = getUrlParameter("utm_position_type");
+let utm_source_type = getUrlParameter("utm_source_type");
+let yclid = getUrlParameter("yclid");
+
+// Устанавливаем куки с UTM-метками
+if (utm_source !== null && utm_source !== "") {
+  document.cookie = `utm_source=${encodeURIComponent(
+    utm_source
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_medium !== null && utm_medium !== "") {
+  document.cookie = `utm_medium=${encodeURIComponent(
+    utm_medium
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_campaign !== null && utm_campaign !== "") {
+  document.cookie = `utm_campaign=${encodeURIComponent(
+    utm_campaign
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_term !== null && utm_term !== "") {
+  document.cookie = `utm_term=${encodeURIComponent(
+    utm_term
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_content !== null && utm_content !== "") {
+  document.cookie = `utm_content=${encodeURIComponent(
+    utm_content
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_campaign_name !== null && utm_campaign_name !== "") {
+  document.cookie = `utm_campaign_name=${encodeURIComponent(
+    utm_campaign_name
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_placement !== null && utm_placement !== "") {
+  document.cookie = `utm_placement=${encodeURIComponent(
+    utm_placement
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_device !== null && utm_device !== "") {
+  document.cookie = `utm_device=${encodeURIComponent(
+    utm_device
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_region_name !== null && utm_region_name !== "") {
+  document.cookie = `utm_region_name=${encodeURIComponent(
+    utm_region_name
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_position !== null && utm_position !== "") {
+  document.cookie = `utm_position=${encodeURIComponent(
+    utm_position
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_position_type !== null && utm_position_type !== "") {
+  document.cookie = `utm_position_type=${encodeURIComponent(
+    utm_position_type
+  )}; path=/; max-age=2592000`;
+}
+
+if (utm_source_type !== null && utm_source_type !== "") {
+  document.cookie = `utm_source_type=${encodeURIComponent(
+    utm_source_type
+  )}; path=/; max-age=2592000`;
+}
+
+if (yclid !== null && yclid !== "") {
+  document.cookie = `yclid=${encodeURIComponent(
+    yclid
+  )}; path=/; max-age=2592000`;
+}
+
+function getCookie(name) {
+  let names = name + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(names) == 0) {
+      return c.substring(names.length, c.length);
+    }
+  }
+  return "";
+}
+
+function fd(formData) {
+  formData.append("utm_source", getCookie("utm_source") || "");
+  formData.append("utm_medium", getCookie("utm_medium") || "");
+  formData.append("utm_campaign", getCookie("utm_campaign") || "");
+  formData.append("utm_campaign_name", getCookie("utm_campaign_name") || "");
+  formData.append("utm_content", getCookie("utm_content") || "");
+  formData.append("utm_term", getCookie("utm_term") || "");
+  formData.append("utm_placement", getCookie("utm_placement") || "");
+  formData.append("utm_device", getCookie("utm_device") || "");
+  formData.append("utm_region_name", getCookie("utm_region_name") || "");
+  formData.append("utm_position", getCookie("utm_position") || "");
+  formData.append("utm_position_type", getCookie("utm_position_type") || "");
+  formData.append("utm_source_type", getCookie("utm_source_type") || "");
+  formData.append("yclid", getCookie("yclid") || "");
+  formData.append("subject", "Заявка с сайта сабвуферы MK Sound");
+}
+
+function sendForm(form) {
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const btn = event.srcElement.querySelector(".red-btn");
+    btn.disabled = true;
+    btn.textContent = "Отправка...";
+    try {
+      const formData = new FormData(form);
+      let object = {};
+
+      fd(formData);
+      const response = await fetch("https://audiics.ru/sendforms.php", {
+        method: "POST",
+        body: formData,
+      });
+      formData.forEach((value, key) => (object[key] = value));
+      localStorage.setItem("name", object.name);
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.status}`);
+      }
+
+      closeAllModals();
+      btn.disabled = false;
+      window.location.href = "/thanks.html";
+    } catch (error) {
+      console.error("Произошла ошибка при отправке формы.", error);
+      alert("Произошла ошибка при отправке формы.", error);
+    }
+  });
+}
+
 /*
 const cardsData = [
   {
