2ff855c1c9ee09f81afd699d576284ef7e1af9d7
Denis Sergeev <denis-sergeev-85@inbox.ru>
2025-03-24 11:52:18 +0300

    1

--- js/render.js
+++ js/render.js
@@ -1,65 +1,6 @@
-//import { cards } from "./cardArr";
+import { cards } from "./cardArr";
 
-export const cards = [
-  {
-    title: "Жилой комплекс Riviera Santa Monica",
-    class: "Премиум",
-    location: "Таиланд, Паттайя, Джомтьен",
-    area: "29 - 325 м²",
-    price: "62 000 $",
-    about: "34 этажа, 446 квартиры",
-    plan: "1, 2, 3 - комнатные, пентхаусы",
-    deadline: "2025-2028",
-    sea: "1 км",
-    developer: "The Riviera Group Thailand",
-    description:
-      "Комплекс расположен в престижном районе с развитой и близостью к пляжу, а из окон открываются виды на море. Удобное расположение: всего в нескольких минутах езды до пляжа, ключевых объектов инфраструктуры и рядом с основными дорожными развязками. Комплекс идеально подходит как для комфортного отдыха, так и для инвестиций - сдачи в аренду или перепрода",
-    id: "block1",
-    pay: "рассрочка",
-    benefit: "24%",
-    income: "5 400 $",
-    btntitle: "Куда вам отправить презентацию The Riviera Santa Monica?",
-    btnsub: "Презентация The Riviera Santa Monica",
-    images: [
-      "./../images/RivetaSantaMonica/Riviera Santa Monica1.webp",
-      "./../images/RivetaSantaMonica/Riviera Santa Monica3.webp",
-      "./../images/RivetaSantaMonica/Riviera Santa Monica4.webp",
-      "./../images/RivetaSantaMonica/Riviera Santa Monica5.webp",
-    ],
-  },
-  {
-    title: "Гостиничный комплекс Нескучный сад",
-    class: "Премиум",
-    location: "Россия, Краснодарский край, г. Сочи",
-    area: "22 м² - 136 м²",
-    price: "26 200 000 ₽",
-    about: "5 корпусов, 407 номеров",
-    plan: "студии, 1, 2, 3, 4 - комнатные",
-    deadline: "2025",
-    sea: "500 м",
-    developer: "ООО СЗ ЗЕЛЕНАЯ ГОРКА",
-    description:
-      "ГК премиум-класса создан специально для тех, кто ценит уединение и высокий уровень комфорта. Инфраструктура комплекса удивляет: зона  проживания и перспективный инвестиционный актив.",
-    declaration: "https://наш.дом.рф/",
-    id: "block2",
-    pay: "ипотека, рассрочка",
-    benefit: "55%",
-    income: "180 000 ₽",
-    btntitle:
-      "Куда вам отправить презентацию гостиничного комплекса Нескучный сад?",
-    btnsub: "Презентация гостиничного комплекса Нескучный сад",
-
-    images: [
-      "./../images/neskuchny garden/1.webp",
-      "./../images/neskuchny garden/2.webp",
-      "./../images/neskuchny garden/3.webp",
-      "./../images/neskuchny garden/4.webp",
-      "./../images/neskuchny garden/5.webp",
-      "./../images/neskuchny garden/6.webp",
-      "./../images/neskuchny garden/7.webp",
-    ],
-  },
-];
+console.log(cards);
 
 function renderCards(cards) {
   const container = document.getElementById("cards-container"); // Контейнер для карточек
